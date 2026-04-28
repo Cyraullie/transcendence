b057be8ac5d73a787ee44a6f2a745958e82fa37c
cyraullie <cyril.goldenschue@gmail.com>
2026-04-28 16:18:43 +0200

    add friendship table

--- srcs/backend/api/models.py
+++ srcs/backend/api/models.py
@@ -6,3 +6,21 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)  # 🔥 IMPORTANT
 
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    is_online = models.BooleanField(default=False)
+    
+class Friendship(models.Model):
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("accepted", "Accepted"),
+        ("blocked", "Blocked"),
+    ]
+
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_friendships")
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_friendships")
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("from_user", "to_user")
