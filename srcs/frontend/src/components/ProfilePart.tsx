b0f0faa705a8795fb34f0a68d3d8c2fc894cc2a7
Crealex <Alexandre.tomasi80@gmail.com>
2026-05-05 18:13:39 +0200

    added arrow do accordion items + create to version of the component for choice after + remove porfilePart at the old place

--- srcs/frontend/src/components/ProfilePart.tsx
+++ srcs/frontend/src/components/ProfilePart.tsx
@@ -3,19 +3,15 @@ import type { accountT } from "../utils/accountType";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import type { errorT } from "../utils/errorType";
-import { IoIosArrowDown } from "react-icons/io";
 
 export function ProfilePart({ accountCurr }: { accountCurr: accountT }) {
   const [failure, setFailure] = useState(false);
   const [success, setSuccess] = useState(false);
-  const [isUnfold, setIsUnfold] = useState(false);
   const [realAccount, setAccount] = useState<accountT | errorT>({
     code: 0,
     response: "",
   });
   const navigate = useNavigate();
-
-  accountCurr.avatar = "";
 
   async function getProfile() {
     setFailure(false);
@@ -46,50 +42,39 @@ export function ProfilePart({ accountCurr }: { accountCurr: accountT }) {
   }
 
   return (
-    <div className="bordered collapse">
-      <input
-        type="radio"
-        name="profile-radio"
-        onClick={() => setIsUnfold(!isUnfold)}
-      />
-      <div className="collapse-title flex items-center gap-6 justify-center">
-        <h2 className="text-center">Your profile</h2>
-        <IoIosArrowDown className={isUnfold ? "hidden" : "" + "text-2xl"} />
-      </div>
-      <div className="collapse-content">
-        <div className="avatar mt-8 flex-col">
-          <div className="rounded-4xl w-24">
-            <img src={realAccount.avatar} />
-          </div>
-          <p className="text-green-200 font-extrabold my-2 mx-auto">
-            {realAccount.is_online ? "Online" : ""}
-          </p>
+    <div>
+      <div className="avatar mt-8 flex-col">
+        <div className="rounded-4xl w-24">
+          <img src={realAccount.avatar} />
         </div>
-        <table className="mt-5">
-          <tr>
-            <th className="th-profile">Username:</th>
-            <td>{realAccount.username}</td>
-          </tr>
-          <tr>
-            <th className="th-profile">Email:</th>
-            <td>{realAccount.mail}</td>
-          </tr>
-          <tr>
-            <th className="th-profile">Password:</th>
-            <td>
-              *******<a className="link"> change</a>
-            </td>
-          </tr>
-          <tr>
-            <th className="th-profile">Joined on:</th>
-            <td>{realAccount.date_joined}</td>
-          </tr>
-          <tr>
-            <th className="th-profile">Last login:</th>
-            <td>{realAccount.is_online ? "now" : realAccount.last_login}</td>
-          </tr>
-        </table>
+        <p className="text-green-200 font-extrabold my-2 mx-auto">
+          {realAccount.is_online ? "Online" : ""}
+        </p>
       </div>
+      <table className="mt-5">
+        <tr>
+          <th className="th-profile">Username:</th>
+          <td>{realAccount.username}</td>
+        </tr>
+        <tr>
+          <th className="th-profile">Email:</th>
+          <td>{realAccount.mail}</td>
+        </tr>
+        <tr>
+          <th className="th-profile">Password:</th>
+          <td>
+            *******<a className="link"> change</a>
+          </td>
+        </tr>
+        <tr>
+          <th className="th-profile">Joined on:</th>
+          <td>{realAccount.date_joined}</td>
+        </tr>
+        <tr>
+          <th className="th-profile">Last login:</th>
+          <td>{realAccount.is_online ? "now" : realAccount.last_login}</td>
+        </tr>
+      </table>
     </div>
   );
 }
