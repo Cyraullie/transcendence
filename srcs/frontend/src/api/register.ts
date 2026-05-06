81b264fad009f4eb19a2d56077caa2bb6700aae3
Wildnachos <167515865+Wildnachos@users.noreply.github.com>
2026-05-06 10:42:41 +0200

    merge from website

--- srcs/frontend/src/api/register.ts
+++ srcs/frontend/src/api/register.ts
@@ -9,12 +9,13 @@ export async function registerRequest(in_email:string, in_user:string, in_pass:s
 	formData.set('email', in_email);
 	formData.set('username', in_user);
 	formData.set('password', in_pass);
+	formData.set('avatar', in_avatar)
 
 	try {
-		const img_response = await fetch(in_avatar);
-		const blob = await img_response.blob();
-		const img = new File([blob], "avatar.png", { type: "image/png"});
-		formData.set('avatar', img);
+		// const img_response = await fetch(in_avatar);
+		// const blob = await img_response.blob();
+		// const img = new File([blob], "avatar.png", { type: "image/png"});
+		// formData.set('avatar', img);
 		const res = await axios.post('http://' + host.host_ip + ':8000/register/', formData,);
 		const response : accessT = {
 			access: res.data['access'],
