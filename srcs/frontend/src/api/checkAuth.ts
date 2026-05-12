622e7400cce37b7bf1278412560bb47e31303d1b
Wildnachos <167515865+Wildnachos@users.noreply.github.com>
2026-05-12 17:31:03 +0200

    navbar fix

--- srcs/frontend/src/api/checkAuth.ts
+++ srcs/frontend/src/api/checkAuth.ts
@@ -1,16 +1,13 @@
 import axios from 'axios'
 import host from '../api/host'
-import status from '../api/login_status'
 
 export async function refreshAuth() : Promise<boolean> {
 
 	try {
 		const res = await axios.post('http://' + host.host_ip + ':8000/api/token/refresh/', { 'refresh': localStorage.getItem('refresh')});
 		localStorage.setItem('access', res.data['access']);
-		status.logged_in = true;
 		return true;
 	} catch {
-		status.logged_in = false;
 		localStorage.removeItem('access');
 		localStorage.removeItem('refresh');
 		return false;
@@ -21,15 +18,12 @@ export async function checkAuth() : Promise<boolean> {
 
 	try {
 		await axios.post('http://' + host.host_ip + ':8000/api/token/verify/', { 'token': localStorage.getItem('access')});
-		status.logged_in = true;
 		return true;
 	} catch {
 		const res = await refreshAuth();
 		if (!res) {
-			status.logged_in = false;
 			return false;
 		}
-		status.logged_in = true;
 		return true;
 	}
 }
