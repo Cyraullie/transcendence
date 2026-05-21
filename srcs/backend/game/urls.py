38e0a27c6f257be8c6b86e85fcccbeb3fefac2c9
cyraullie <cyril.goldenschue@gmail.com>
2026-05-21 14:25:21 +0200

    add list of open room with friends inside

--- srcs/backend/game/urls.py
+++ srcs/backend/game/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views.room_views import create_room, add_bot, list_public_room
+from .views.room_views import create_room, add_bot, list_public_room, list_friend_room
 
 
 urlpatterns = [
     path('rooms/', create_room, name='create-room'),
     path('rooms/<str:code>/add_bot/', add_bot, name='add-bot'),
-    path('rooms/public/', list_public_room, name='add-bot'),
+    path('rooms/public/', list_public_room),
+    path('rooms/friend/', list_friend_room),
 ]
--- srcs/backend/game/views/room_views.py
+++ srcs/backend/game/views/room_views.py
@@ -4,8 +4,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from api.auth.authentication import OptionalJWTAuthentication
 from ..models import Room, PlayerPresence
-from api.models import User
+from api.models import User, Friendship
 from ..serializers import RoomSerializer
+from django.db.models import Q
 from ..db import add_bot_to_room
 import uuid
 
@@ -107,4 +108,73 @@ def list_public_room(request):
     
     return Response(data, status=200)
 
-#TODO list of room with friends inside
+@api_view(["GET"])
+@authentication_classes([OptionalJWTAuthentication])
+@permission_classes([IsAuthenticated])
+def list_friend_room(request):
+
+    friendships = Friendship.objects.filter(
+        Q(from_user=request.user) | Q(to_user=request.user),
+        status="accepted"
+    )
+
+    friends = set()
+
+    for f in friendships:
+        if f.from_user == request.user:
+            friends.add(f.to_user)
+        else:
+            friends.add(f.from_user)
+
+    rooms = Room.objects.filter(
+        is_private=0,
+        status="open"
+    )
+
+    presences = PlayerPresence.objects.filter(
+        room__in=rooms
+    ).select_related("player", "room")
+
+    room_map = {}
+
+    for p in presences:
+
+        room_id = p.room.id
+
+        if room_id not in room_map:
+            room_map[room_id] = {
+                "room": p.room,
+                "players": [],
+                "has_friend": False
+            }
+
+        room_map[room_id]["players"].append(p)
+
+        if p.player in friends:
+            room_map[room_id]["has_friend"] = True
+
+    data = []
+
+    for value in room_map.values():
+
+        if not value["has_friend"]:
+            continue
+
+        room = value["room"]
+
+        list_player = [
+            {
+                "username": p.player.username
+            }
+            for p in value["players"]
+        ]
+
+        data.append({
+            "id": room.id,
+            "code": room.code,
+            "nb_player": room.nb_player,
+            "list_player": list_player,
+            "host": room.host.username if room.host else None,
+        })
+
+    return Response(data, status=200)
