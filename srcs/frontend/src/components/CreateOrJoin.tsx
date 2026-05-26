5f4ab37001e92d6c8279b342199b7477a3864fcc
Crealex <Alexandre.tomasi80@gmail.com>
2026-05-26 19:04:41 +0200

    created fake games availables + started table games availables

--- srcs/frontend/src/components/CreateOrJoin.tsx
+++ srcs/frontend/src/components/CreateOrJoin.tsx
@@ -1,27 +1,51 @@
+import { FaPlay } from "react-icons/fa";
+import { createFakeGame } from "../utils/test_funcs/createFakeGameAvailable";
+
 export default function CreateOrJoin() {
+  const availableGames = createFakeGame();
   return (
     <div className="page-content mt-17">
       <h1 className="">Game</h1>
-      <div className="container w-full">
-        <div className="createOrJoinBtn-container w-1/3 text-center mt-10">
+      <div className="w-full flex h-full">
+        <div className="createOrJoinBtn-container w-1/3 flex flex-col my-auto items-center ">
           <button className="btn m-3">Create</button>
-		  <p>or</p>
-		  <div className="joinCode-container m-3">
-		  <input className="input w-2/3 m-3" type="text" placeholder="enter game code here"/>
-		  <button className="btn">Join</button>
-		  </div>
+          <p>or</p>
+          <div className="joinCode-container m-3">
+            <input
+              className="input w-2/3 m-3"
+              type="text"
+              placeholder="enter game code here"
+            />
+            <button className="btn">Join</button>
+          </div>
         </div>
-        <div className="listAvailableGame w-2/3">
-		<table className="table">
-		<thead>
-	<tr>
-	<th>Room</th>
-	<th>nb of player/max player</th>
-	<th>pending</th>
-	</tr>
-		</thead>
-		</table>
-		</div>
+        <div className="listAvailableGame w-2/3 flex justify-center bordered h-fit max-h-8/12 m-6">
+          <table className="table ml-10">
+            <thead>
+              <tr>
+                <th>Room</th>
+                <th>nb of player/max player</th>
+                <th>pending</th>
+              </tr>
+            </thead>
+            <tbody>
+              {availableGames.map((game) => {
+                return (
+                  <tr>
+                    <td>{game.host}'s room</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                      <button className="btn">
+                        <FaPlay />{" "}
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
--- srcs/frontend/src/utils/test_funcs/createFakeGameAvailable.tsx
+++ srcs/frontend/src/utils/test_funcs/createFakeGameAvailable.tsx
@@ -1,20 +1,149 @@
 import type { availableGameT } from "../availableGameType";
 
 export function createFakeGame(): availableGameT[] {
-	return [
-		{
-			id: 0,
-			code: "caca123",
-			nb_player: 3,
-			list_player: [
-				{
-					username: "philou",
-				},
-				{
-					username: "phistule",
-				},
-			],
-
-		}
-	]
+  return [
+    {
+      id: 0,
+      code: "phil123",
+      nb_player: 7,
+      list_player: [
+        {
+          username: "philou",
+        },
+        {
+          username: "phistule",
+        },
+        {
+          username: "philipe",
+        },
+        {
+          username: "phibonacci",
+        },
+        {
+          username: "phiphi",
+        },
+        {
+          username: "phuck",
+        },
+      ],
+      host: "philipe",
+    },
+    {
+      id: 1,
+      code: "fred123",
+      nb_player: 4,
+      list_player: [
+        {
+          username: "fredo",
+        },
+        {
+          username: "frederic",
+        },
+        {
+          username: "fredouane",
+        },
+        {
+          username: "fredpayment",
+        },
+      ],
+      host: "frederic",
+    },
+    {
+      id: 2,
+      code: "anne123",
+      nb_player: 5,
+      list_player: [
+        {
+          username: "anna",
+        },
+        {
+          username: "annesthesie",
+        },
+        {
+          username: "annick",
+        },
+      ],
+      host: "anna",
+    },
+    {
+      id: 3,
+      code: "clem123",
+      nb_player: 6,
+      list_player: [
+        {
+          username: "clemence",
+        },
+        {
+          username: "clementine",
+        },
+        {
+          username: "clement",
+        },
+        {
+          username: "clemercestcquiyadplubo",
+        },
+      ],
+      host: "clementine",
+    },
+    {
+      id: 4,
+      code: "al123",
+      nb_player: 3,
+      list_player: [
+        {
+          username: "alain",
+        },
+        {
+          username: "alapecheaumoulemoulemoulejenveuxplusyallermaman",
+        },
+        {
+          username: "alana",
+        },
+      ],
+      host: "alana",
+    },
+    {
+      id: 5,
+      code: "six123",
+      nb_player: 6,
+      list_player: [
+        {
+          username: "sixtine",
+        },
+        {
+          username: "sixto",
+        },
+        {
+          username: "sixta",
+        },
+        {
+          username: "sixte",
+        },
+        {
+          username: "sixseveeeeeeeeen",
+        },
+      ],
+      host: "sixte",
+    },
+    {
+      id: 6,
+      code: "ki123",
+      nb_player: 4,
+      list_player: [
+        {
+          username: "kirikou",
+        },
+        {
+          username: "kilian",
+        },
+        {
+          username: "kishta",
+        },
+        {
+          username: "kicestquiecritcesprenomdemerde",
+        },
+      ],
+      host: "kirikou",
+    },
+  ];
 }
