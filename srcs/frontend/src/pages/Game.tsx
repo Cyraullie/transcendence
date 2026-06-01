c4a700a73cd8a17e50845bb0bd3c54c224527e22
neoo-n <dana.vauthey@gmail.com>
2026-06-01 15:30:23 +0200

    footer linked hover

--- srcs/frontend/src/pages/Game.tsx
+++ srcs/frontend/src/pages/Game.tsx
@@ -2,7 +2,7 @@ import CreateOrJoin from "../components/Game/CreateOrJoin";
 import WaitingRoom from "../components/waitingRoom/WaitingRoom";
 
 export function Game({logged_in} : {logged_in : boolean}) {
-	const isInWaitingRoom = false;
+	const isInWaitingRoom = true;
   return (
 	  !isInWaitingRoom ? <CreateOrJoin /> : <WaitingRoom logged_in={logged_in}/>
   );
