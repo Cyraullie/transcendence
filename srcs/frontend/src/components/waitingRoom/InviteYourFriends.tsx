30be5d7e7ad1e0100cd6282ea0b12d267449ce72
neoo-n <dana.vauthey@gmail.com>
2026-06-01 14:43:15 +0200

    oupsi

--- srcs/frontend/src/components/waitingRoom/InviteYourFriends.tsx
+++ srcs/frontend/src/components/waitingRoom/InviteYourFriends.tsx
@@ -1,25 +1,50 @@
+import { useRef } from "react";
 import generateFakePlayerList from "../../utils/test_funcs/generateFakePlayerList";
 
 export default function InviteYourFriends() {
-  const fakeFriendsList = generateFakePlayerList();
+  const showFriendsList = useRef<HTMLDialogElement>(null);
+  const fakePlayers = generateFakePlayerList();
 
   return (
     <>
-    <button
-      className="btn "
-    >
-      hi
-    </button>
-    <div className="modal-box bg-(--nav-color)">
-      <div className="flex">
-        <p>hi</p>
-      </div>
-      {/* {* if friend *} 
-				 need to modify a lot of thing here like the width of the modal ( surement creer un nouveau component history) */}
-    </div>
+      <button
+        className="btn "
+        onClick={() => showFriendsList.current?.showModal()}
+      >
+        Invite your friends
+      </button>
+      <dialog id="showFriendsList" className="modal" ref={showFriendsList}>
+        <div className="modal-box bg-(--nav-color) w-1/4">
+          <table className="table">
+            <thead>
+              <th>Invite your friends</th>
+            </thead>
+            <tbody>
+              {fakePlayers.map((friend) => (
+                <tr>
+                  <td>
+                    {friend.username}
+                  </td>
+                  <td>
+                    <label className="swap btn">
+                      <input type="checkbox" />
+                      <button className="swap-off">
+                        Invite
+                      </button>
+                      <button className="swap-on">
+                        Done
+                      </button>
+                    </label>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       <form method="dialog" className="modal-backdrop">
         <button ></button>
       </form>
+      </dialog>
     </>
   );
 }
