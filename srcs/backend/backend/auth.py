4d91a194b6aeb423db084d6eab1bdcfb6531c93a
Wildnachos <167515865+Wildnachos@users.noreply.github.com>
2026-06-04 09:39:56 +0200

    check auth fixed (I can't spell)

--- srcs/backend/backend/auth.py
+++ srcs/backend/backend/auth.py
@@ -11,7 +11,7 @@ from django.conf import settings
 @authentication_classes([OptionalJWTAuthentication])
 def VerifyCookie(request):
 	if request.user.is_authenticated:
-		return Response({"status":"succes"})
+		return Response({"status":"success"})
 	else:
 		return Response({"status":"failed"})
 
