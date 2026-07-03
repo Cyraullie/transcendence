5ae85e4201c4855c9a8c9c440be0fa9ca9d3fdcb
creAlex <Alexandre.tomasi80@gmail.com>
2026-07-03 10:08:58 +0200

    comment fakeHistory

--- srcs/frontend/src/components/miniProfile/MiniHistory.tsx
+++ srcs/frontend/src/components/miniProfile/MiniHistory.tsx
@@ -14,61 +14,61 @@ type Props = {
 export function MiniHistory({ history, updatedProfile, setUpdate }: Props) {
   const [isMore, setIsMore] = useState(false);
   const [nbSlice, setNbSlice] = useState(10);
-  const fakeHistory = [
-    {
-      game_id: 0,
-      start: "dnflds",
-      points: 4,
-      rank: 3,
-      won: true,
-      duration: 122,
-      nb_player: 2,
-      players: [
-        {
-          id: 11,
-          username: "philipdjasdhdkjahdkjsakjhasjkas",
-          is_host: true,
-          position: 1,
-        },
-        {
-          id: 1,
-          username: "philip2",
-          is_host: false,
-          position: 1,
-        },
-      ]
-    },
-
-    {
-      game_id: 1,
-      start: "dnflds",
-      points: 4,
-      rank: 3,
-      won: false,
-      duration: 122,
-      nb_player: 2,
-      players: [
-        {
-          id: 0,
-          username: "philipe",
-          is_host: true,
-          position: 1,
-        },
-        {
-          id: 1,
-          username: "philip2",
-          is_host: false,
-          position: 1,
-        },
-        {
-          id: 2,
-          username: "philip3",
-          is_host: false,
-          position: 2,
-        },
-      ]
-    }
-  ]
+  // const fakeHistory = [
+  //   {
+  //     game_id: 0,
+  //     start: "dnflds",
+  //     points: 4,
+  //     rank: 3,
+  //     won: true,
+  //     duration: 122,
+  //     nb_player: 2,
+  //     players: [
+  //       {
+  //         id: 11,
+  //         username: "philipdjasdhdkjahdkjsakjhasjkas",
+  //         is_host: true,
+  //         position: 1,
+  //       },
+  //       {
+  //         id: 1,
+  //         username: "philip2",
+  //         is_host: false,
+  //         position: 1,
+  //       },
+  //     ]
+  //   },
+  //
+  //   {
+  //     game_id: 1,
+  //     start: "dnflds",
+  //     points: 4,
+  //     rank: 3,
+  //     won: false,
+  //     duration: 122,
+  //     nb_player: 2,
+  //     players: [
+  //       {
+  //         id: 0,
+  //         username: "philipe",
+  //         is_host: true,
+  //         position: 1,
+  //       },
+  //       {
+  //         id: 1,
+  //         username: "philip2",
+  //         is_host: false,
+  //         position: 1,
+  //       },
+  //       {
+  //         id: 2,
+  //         username: "philip3",
+  //         is_host: false,
+  //         position: 2,
+  //       },
+  //     ]
+  //   }
+  // ]
 
 
   if ('code' in history) {
@@ -102,7 +102,7 @@ export function MiniHistory({ history, updatedProfile, setUpdate }: Props) {
         Show history of Eude
       </div>
       <div className="collapse-content text-sm z-1">
-        {fakeHistory.length === 0 ?
+        {history.length === 0 ?
           <div className="text-center">
             <p>Once you have played a game, you'll be able to see it here !</p>
           </div>
@@ -116,7 +116,7 @@ export function MiniHistory({ history, updatedProfile, setUpdate }: Props) {
               <th className="th-history">Nb players</th>
               <th className=" overflow-hidden">Opponents</th>
             </tr>
-            {fakeHistory.slice(0, nbSlice).map((game: historyT) => (
+            {history.slice(0, nbSlice).map((game: historyT) => (
 
               <tr
                 className={
