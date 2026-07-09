9db316240477741a3df19653ecb1d76061b50b87
neoo-n <dana.vauthey@gmail.com>
2026-07-09 11:27:06 +0200

    lighting with better contrast on assets

--- srcs/frontend/src/components/Game/Game/GameVisual.tsx
+++ srcs/frontend/src/components/Game/Game/GameVisual.tsx
@@ -3,10 +3,9 @@ import Hand from "./GameVisual/Hand";
 import Board from "./GameVisual/Board";
 import generateDeck from "../../../utils/createDeck";
 import { loadTexture } from "../../../utils/imports/textures";
-import { AxesHelper, TextureLoader } from "three";
-import * as THREE from "three";
+import { TextureLoader } from "three";
 
-// import { OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 const bgimg = "/assets/bg_game.png"
 
 export default function GameVisual() {
@@ -21,14 +20,14 @@ export default function GameVisual() {
   const distanceBoard = boardRadius * 3 / 5;
 
   return (
-    <Canvas className="w-3/4 bg-cover rounded-2xl"  style={{ backgroundImage: `url(${bgimg})` }}>
-      <axesHelper/>
-      {/* <directionalLight position={[0, 5, 0]} intensity={1.2} color={"#454988"}/>
-      <spotLight position={[0, 5, 5]} intensity={20} angle={10} color={"#c7a089"}/> */}
-      <ambientLight intensity={0.1} color={"#ffffff"}/>
+    <Canvas className="w-3/4 bg-cover rounded-2xl" linear={true} style={{ backgroundImage: `url(${bgimg})` }}>
+      <axesHelper />
+      <directionalLight position={[0, 10, 7]} intensity={1.2} color={"#ffe5d5"} />
+      <pointLight position={[0, 5, 5]} intensity={10} color={"#ffffff"} />
+      <ambientLight />
       <Board front={cardsTex} back={back} boardRadius={boardRadius} distanceBoard={distanceBoard} />
       <Hand cardsTex={cardsTex} back={back} distanceBoard={distanceBoard} />
-      {/* <OrbitControls /> */}
+      <OrbitControls />
     </Canvas>
   );
 }
