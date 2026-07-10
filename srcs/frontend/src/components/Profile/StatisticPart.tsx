56953933507ba7adf6c71d0b513d46ecf2f649b0
creAlex <Alexandre.tomasi80@gmail.com>
2026-07-10 18:13:30 +0200

    finished stats responsive

--- srcs/frontend/src/components/Profile/StatisticPart.tsx
+++ srcs/frontend/src/components/Profile/StatisticPart.tsx
@@ -14,34 +14,34 @@ export function StatisticsPart({stats}:{stats:statisticsT}) {
         </td>
       </tr>
 	  <tr className="md:hidden">
-        <th className="th-stats ">Winrate(W/L): </th>
+        <th className="th-stats ">Winrate: </th>
         <td className="">
-          {( stats.played > 0 ? (stats.win / stats.played).toPrecision(3): 0)} ({stats.win}/{" "}
-          {stats.lose})
+          {( stats.played > 0 ? (stats.win / stats.played).toPrecision(3): 0)}
         </td>
 	  </tr>
       <tr>
-        <th className="th-stats">Total points received:</th>
+        <th className="th-stats max-md:hidden">Points received:</th>
+        <th className="th-stats md:hidden">Points received:</th>
         <td>{stats.total_points}</td>
-        <th className="th-stats max-md:hidden">Times tricks choosen</th>
+        <th className="th-stats max-md:hidden">Times tricks chosen:</th>
         <td className="max-md:hidden">{stats.nb_trick_choose}</td>
       </tr>
 	  <tr className="md:hidden">
-        <th className="th-stats">Times tricks choosen:</th>
+        <th className="th-stats">Times tricks chosen:</th>
         <td>{stats.nb_trick_choose}</td>
 	  </tr>
       <tr>
-        <th className="th-stats">Prefered tricks</th>
+        <th className="th-stats">Preferred tricks:</th>
         <td>{stats.prefered_trick !== "null" ? stats.prefered_trick : "none"}</td>
-        <th className="th-stats max-md:hidden">Times fold taken</th>
+        <th className="th-stats max-md:hidden">Folds taken:</th>
         <td className="max-md:hidden">{stats.nb_taken}</td>
       </tr>
 	  <tr className="md:hidden">
-        <th className="th-stats">Times fold taken:</th>
+        <th className="th-stats">Folds taken:</th>
         <td>{stats.nb_taken}</td>
 	  </tr>
       <tr>
-        <th className="th-stats">Times last fold taken:</th>
+        <th className="th-stats">Last folds taken:</th>
         <td>{stats.nb_last_take}</td>
         <th className="th-stats max-md:hidden">Total hand meld points: </th>
         <td className="max-md:hidden">{stats.hand_meld_points}</td>
@@ -53,7 +53,7 @@ export function StatisticsPart({stats}:{stats:statisticsT}) {
       <tr>
         <th className="th-stats">Total board meld points: </th>
         <td>{stats.board_meld_points}</td>
-        <th className="th-stats max-md:hidden">Highest hand meld</th>
+        <th className="th-stats max-md:hidden">Highest hand meld:</th>
         <td className="max-md:hidden">{stats.highest_hand_meld}</td>
       </tr>
 	  <tr className="md:hidden">
@@ -63,11 +63,11 @@ export function StatisticsPart({stats}:{stats:statisticsT}) {
       <tr>
         <th className="th-stats">Highest board meld:</th>
         <td>{stats.highest_board_meld}</td>
-        <th className="th-stats max-md:hidden">Total hosting game</th>
+        <th className="th-stats max-md:hidden">Game as host:</th>
         <td className="max-md:hidden">{stats.nb_host}</td>
       </tr>
 	  <tr className="md:hidden">
-        <th className="th-stats">Total hosting game:</th>
+        <th className="th-stats">Game as host:</th>
         <td>{stats.nb_host}</td>
 	  </tr>
     </table>
